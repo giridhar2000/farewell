87d554cfcbe3e0df5fa4725d66f76b69c732f900
GiridharHUB <scintillantmail@gmail.com>
2023-09-10 09:50:54 +0530

    changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,60 @@
 import React from 'react'
-import ScrollPage from '../Scroll/ScrollPage'
-import { HScroll } from '../HorizontalScroll/HScroll'
-
+import Marquee from "react-fast-marquee";
+import img1 from "../../Assets/img/img1.jpeg"
+import img2 from "../../Assets/img/img2.jpeg"
+import img3 from "../../Assets/img/img3.jpeg"
+import img4 from "../../Assets/img/img4.jpeg"
+import img5 from "../../Assets/img/img5.jpeg"
+import img6 from "../../Assets/img/img6.jpeg"
+import img7 from "../../Assets/img/img7.jpeg"
+import img8 from "../../Assets/img/img8.jpeg"
+import img9 from "../../Assets/img/img9.jpeg"
+import img10 from "../../Assets/img/img10.jpeg"
+import "../Home/Home.css"
 
 export default function Home() {
   return (
-    <div style={{width: "100%"}}>
-      {/* <ScrollPage /> */}
-      <HScroll />
+    <div style={{ width: "100%" }}>
+
+      <div className='marquees'>
+        <Marquee>
+          <div>
+            <img src={img1} alt='img' />
+          </div>
+          <div>
+            <img src={img2} alt='img' />
+          </div>
+          <div>
+            <img src={img3} alt='img' />
+          </div>
+          <div>
+            <img src={img4} alt='img' />
+          </div>
+          <div>
+            <img src={img5} alt='img' />
+          </div>
+        </Marquee>
+      </div>
+      <div>
+        <Marquee className='marquees'>
+          <div>
+            <img src={img6} alt='img' />
+          </div>
+          <div>
+            <img src={img7} alt='img' />
+          </div>
+          <div>
+            <img src={img8} alt='img' />
+          </div>
+          <div>
+            <img src={img9} alt='img' />
+          </div>
+          <div>
+            <img src={img10} alt='img' />
+          </div>
+
+        </Marquee>
+      </div>
     </div>
   )
 }
--- src/Components/Travel/Travel.js
+++ src/Components/Travel/Travel.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import travel from "../../Assets/img/travel.jpg"
 
 export default function Travel() {
   return (
     <div className='container'>
-        <iframe title='HotelShaans' height="700" frameborder="2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.9146634597696!2d78.68249791531633!3d10.817842461384716!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3baaf5a0bb9da3d1%3A0x26ad0a730ae37949!2sHotel%20Shaans!5e0!3m2!1sen!2sin!4v1656422100093!5m2!1sen!2sin"></iframe>
+        {/* <iframe title='HotelShaans' height="700" frameborder="2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.9146634597696!2d78.68249791531633!3d10.817842461384716!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3baaf5a0bb9da3d1%3A0x26ad0a730ae37949!2sHotel%20Shaans!5e0!3m2!1sen!2sin!4v1656422100093!5m2!1sen!2sin"></iframe> */}
     </div>
   )
 }
